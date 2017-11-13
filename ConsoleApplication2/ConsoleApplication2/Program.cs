0628b54719e6aa70e2ee5e82fcf3e0608163174e
l2 <justaaleksandra@gmail.com>
2017-11-13 20:03:49 +0100

    CRC

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CRC
+{
+    internal class Program
+    {
+
+
+    public static void Main(string[] args)
+    {
+           
+
+    byte b, a;
+    byte m;
+    char[] cos2 = new char[] {'d', 'g', 't', 'r'};
+    a = 0;
+    foreach (var v in cos2)
+
+
+    {
+        for (int j = 0; j < 8; j++)
+        {
+
+            m = (byte) (1 << (j - 1));
+            b = (byte) ((v & m) >> (j - 1));
+            a ^= b;
+        }
+    }
+    Console.WriteLine(a);
+    }
+}
+    
+}
